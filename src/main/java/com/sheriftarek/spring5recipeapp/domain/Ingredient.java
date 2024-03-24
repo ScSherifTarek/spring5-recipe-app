438d82c9211dcc603af7ba238c20afbb3b24a4fd
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-03-24 15:46:55 +0300

    Fix Ingredient to UOM relationship

--- src/main/java/com/sheriftarek/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/sheriftarek/spring5recipeapp/domain/Ingredient.java
@@ -6,13 +6,12 @@ import java.math.BigDecimal;
 
 @Entity
 public class Ingredient {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private BigDecimal amount;
-    @OneToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
     @ManyToOne
