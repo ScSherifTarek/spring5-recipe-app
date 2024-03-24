c7c9af3cd5d8939c0f052dbb6bbe0669b1c3e339
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-03-24 14:51:09 +0300

    Make UOM id generation strategy as IDENTITY

--- src/main/java/com/sheriftarek/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/com/sheriftarek/spring5recipeapp/domain/UnitOfMeasure.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class UnitOfMeasure {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
 
