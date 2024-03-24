d8a5780a2a2859a93d7d2849e27b864794cf8e85
Sherif Tarek <sherif.tarek.software.engineer@gmail.com>
2024-03-24 15:26:54 +0300

    Add findByDescription methods to Catgegory and UnitOfMeasure Repositories

--- src/main/java/com/sheriftarek/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/sheriftarek/spring5recipeapp/repositories/CategoryRepository.java
@@ -4,6 +4,9 @@ import com.sheriftarek.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
